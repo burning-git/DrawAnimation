c21d1877ef3f16bf8133499892dffed11b1d7b6a
burning_git <974201360@qq.com>
2014-12-01 16:54:12 +0800

    Initial Commit

---
+++ DrawAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawAnimation
+//
+//  Created by gitBurning on 14/12/1.
+//  Copyright (c) 2014年 gitBurning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawAnimation
+//
+//  Created by gitBurning on 14/12/1.
+//  Copyright (c) 2014年 gitBurning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
